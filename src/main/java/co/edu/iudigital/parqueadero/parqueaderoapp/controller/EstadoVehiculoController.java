1686c6422a251464bd472e462aac512402fed902
Daniela Rodriguez <87885394+DRodriguez04@users.noreply.github.com>
2022-11-07 12:58:43 -0500

    Update EstadoVehiculoController.java

--- src/main/java/co/edu/iudigital/parqueadero/parqueaderoapp/controller/EstadoVehiculoController.java
+++ src/main/java/co/edu/iudigital/parqueadero/parqueaderoapp/controller/EstadoVehiculoController.java
@@ -1,5 +1,38 @@
 package co.edu.iudigital.parqueadero.parqueaderoapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.iudigital.parqueadero.parqueaderoapp.domain.EstadoVehiculo;
+import co.edu.iudigital.parqueadero.parqueaderoapp.services.EstadoVehiculoService;
+
+@RestController
+@RequestMapping("/estado-vehiculo")
+@CrossOrigin("*")
 public class EstadoVehiculoController {
+
+  
+  @Autowired
+  private EstadoVehiculoService estadoVehiculoService;
+  
+  @PostMapping
+  @ResponseStatus(HttpsStatus.CREATED)
+  public void regEstVehiculo(@RequestBody EstadoVehiculo estadoVehiculo){
+    estadoVehiculoService.regEstVehiculo(estadoVehiculo);
+  }
+
+  @PutMapping("/{estado}")
+  @ResponseStatus(HttpStatus.CREATED)
+  public void actEstVehiculo(@PathVariable("estado") String estado, @RequestBody EstadoVehiculo estadoVehiculo){
+    estadoVehiculoService.updEstadoVeh(estadoVehiculo);
+  }
 
 }
