96eb6cf677a4558694ec786bd3dcf0292ee7d1d9
Daniela Rodriguez <rodriguezberriod@gmail.com>
2022-11-06 23:47:49 -0500

    segundo commit del código.

--- src/main/java/co/edu/iudigital/parqueadero/parqueaderoapp/repository/VehiculoRepository.java
+++ src/main/java/co/edu/iudigital/parqueadero/parqueaderoapp/repository/VehiculoRepository.java
@@ -1,5 +1,7 @@
 package co.edu.iudigital.parqueadero.parqueaderoapp.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import co.edu.iudigital.parqueadero.parqueaderoapp.domain.Vehiculo;
 @Repository
 public interface VehiculoRepository extends CrudRepository<Vehiculo, Integer> {
 
+    static Optional<Vehiculo> findByPlaca(String placa) {
+        return null;
+    }
 }
--- src/main/java/co/edu/iudigital/parqueadero/parqueaderoapp/services/VehiculoService.java
+++ src/main/java/co/edu/iudigital/parqueadero/parqueaderoapp/services/VehiculoService.java
@@ -1,6 +1,7 @@
 package co.edu.iudigital.parqueadero.parqueaderoapp.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,19 @@ public class VehiculoService {
     public List<Vehiculo> getVehiculos() {
         List<Vehiculo> vehiculo = (List<Vehiculo>) vehiculoRepository.findAll();
         return vehiculo;
+    }
+
+    public Optional<Vehiculo> gVehOptional(int id_vehiculo) {
+        Optional<Vehiculo> gVehById = vehiculoRepository.findById(id_vehiculo);
+        return gVehById;
+    }
+
+    public Optional<Vehiculo> gVehPlaca(String placa) {
+        Optional<Vehiculo> gVeByPlaca = VehiculoRepository.findByPlaca(placa);
+        return gVeByPlaca;
+    }
+
+    public void delVehiculo(int id_vehiculo) {
+        vehiculoRepository.deleteById(id_vehiculo);
     }
 }
