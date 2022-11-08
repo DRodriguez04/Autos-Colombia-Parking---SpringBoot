4f1dd267aa26ec00f3be0000371e745baf074134
Daniela Rodriguez <rodriguezberriod@gmail.com>
2022-11-07 22:07:36 -0500

    GestVehController importacion de lib en repo local

--- src/main/java/co/edu/iudigital/parqueadero/parqueaderoapp/controller/GestionVehiculoController.java
+++ src/main/java/co/edu/iudigital/parqueadero/parqueaderoapp/controller/GestionVehiculoController.java
@@ -1,12 +1,15 @@
 package co.edu.iudigital.parqueadero.parqueaderoapp.controller;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,26 +35,29 @@ public class GestionVehiculoController {
     }
 
     @GetMapping
-    public List<GestionVehiculo> getListGestion() {
+    public List<GestionVehiculo> gettingListGestion() {
         List<GestionVehiculo> gestVehiculo = new ArrayList<>();
         return gestVehiculo;
     }
-    
+
     @GetMapping("/{id_registro}")
-    public Optional<GestionVehiculo> getGestionId(@PathVariable int id_registro){
+    public Optional<GestionVehiculo> gettingGestionId(@PathVariable int id_registro) {
         return gestionVehiculoService.gOptional(id_registro);
     }
-    
+
     @PutMapping("/{fechaIngreso}")
     @ResponseStatus(HttpStatus.CREATED)
-    public void actfechaIngresoVehiculo(@PathVariable("fechaIngreso") LocalDateTime fechaIngreso, @RequestBody GestionVehiculo gestionVehiculo) {
+    public void actfechaIngresoVehiculo(@PathVariable("fechaIngreso") LocalDateTime fechaIngreso,
+            @RequestBody GestionVehiculo gestionVehiculo) {
         gestionVehiculoService.actfechaIngreso(gestionVehiculo);
     }
-    
+
     @PutMapping("/{fechaSalida}")
     @ResponseStatus(HttpStatus.CREATED)
-    public void actfechaSalidaVehiculo(@PathVariable("fechaSalida") LocalDateTime fechaSalida, @RequestBody GestionVehiculo gestionVehiculo) {
+    public void actfechaSalidaVehiculo(@PathVariable("fechaSalida") LocalDateTime fechaSalida,
+            @RequestBody GestionVehiculo gestionVehiculo) {
         gestionVehiculoService.actfechaSalida(gestionVehiculo);
+
     }
 
 }
